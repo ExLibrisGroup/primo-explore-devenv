8a5b06db80f96066e8f54009fbdc2f2a75eb10c5
Noam Amit <noamamit92@gmail.com>
2017-04-25 11:01:04 +0300

    support --proxy to start the environment with a proxy server defined from the command line

--- gulpfile.js
+++ gulpfile.js
@@ -8,4 +8,5 @@ const config = require('./gulp/config');
 
 var options = minimist(process.argv.slice(2));
 config.setView(options.view);
+config.setProxy(options.proxy);
 config.setBrowserify(options.browserify);
