2e632527113f98b3a1222627bfa12395176e2e5d
barnabyalter <barnaby.alter@nyu.edu>
2017-04-26 23:16:56 -0400

    Override custom-css tasks to compile from SCSS

--- gulp/custom/tasks/compile-css.js
+++ gulp/custom/tasks/compile-css.js
@@ -7,13 +7,25 @@ let gulp = require('gulp');
 let sass = require('gulp-sass');
 let gutil = require('gulp-util');
 let plumber = require('gulp-plumber');
+let concat = require('gulp-concat');
 
-gulp.task('nyu-compile-css', () => {
+// Override custom-css from buildCustomCss.js
+// to compile SCSS down to CSS first and then
+// create the custom1.css file
+gulp.task('custom-css', () => {
   gutil.log("Writing to file " + buildParams.viewCssDir() + "/" + view.toLowerCase() + ".css");
   let srcFile = buildParams.viewCssDir() + "/sass/" + view.toLowerCase() + ".scss";
-  return gulp.src(srcFile)
+  gulp.src(srcFile)
     .pipe(plumber())
     .pipe(sass({errLogToConsole: true}))
     .on('error', config.handleError)
     .pipe(gulp.dest(buildParams.viewCssDir()));
+  gulp.start('default-custom-css');
+});
+
+// Copied from buildCustomCss.js task
+gulp.task('default-custom-css', () => {
+  return gulp.src([buildParams.customCssMainPath(),buildParams.customNpmCssPath(),'!'+buildParams.customCssPath()])
+      .pipe(concat(buildParams.customCssFile))
+      .pipe(gulp.dest(buildParams.viewCssDir()));
 });
