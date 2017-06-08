e3b252c2e04e1f06b7e9c50ad94aef3616021318
ShoulM <shoulma@gmail.com>
2017-06-08 16:54:31 +0300

    fix external template with browserify (#48)
    Fixes #47

--- gulp/tasks/buildCustomHtmlTemplates.js
+++ gulp/tasks/buildCustomHtmlTemplates.js
@@ -3,15 +3,39 @@ const gulp = require('gulp');
 const wrap = require("gulp-wrap");
 const templateCache = require('gulp-angular-templatecache');
 const config = require('../config.js');
+const fs = require('fs');
 
 let buildParams = config.buildParams;
 
+function parseModuleName(){
+    let mainJsContent= fs.readFileSync(buildParams.viewJsDir() + '/main.js', 'utf8');
+    let moduleString= "angular.module('";
+    let index= mainJsContent.indexOf(moduleString) + moduleString.length;
+    mainJsContent= mainJsContent.slice(index);
+    index= mainJsContent.indexOf("'");
+    let module= mainJsContent.slice(0, index);
+    return module;
+}
+
+
+function prepareTempltesWithBrowserify(){
+    let module = parseModuleName();
+    gulp.src(buildParams.viewHtmlDir() + '/templates/**/*.html')
+        .pipe(templateCache({filename:'customTemplates.js', module: module}))
+        .pipe(gulp.dest(buildParams.viewJsDir()));
+}
+
 function prepareTemplates() {
-  gulp.src(buildParams.viewHtmlDir() + '/templates/**/*.html')
-    .pipe(templateCache({filename:'customTemplates.js', templateHeader: 'app.run(function($templateCache) {', templateFooter: '});'}))    
-    .pipe(gulp.dest(buildParams.viewJsDir()));
+    if(config.getBrowserify()){
+        prepareTempltesWithBrowserify();
+    }
+    else{
+        gulp.src(buildParams.viewHtmlDir() + '/templates/**/*.html')
+            .pipe(templateCache({filename:'customTemplates.js', templateHeader: 'app.run(function($templateCache) {', templateFooter: '});'}))
+            .pipe(gulp.dest(buildParams.viewJsDir()));
+    }
 }
 
 gulp.task('custom-html-templates', () => {
-  prepareTemplates();
+    prepareTemplates();
 })
