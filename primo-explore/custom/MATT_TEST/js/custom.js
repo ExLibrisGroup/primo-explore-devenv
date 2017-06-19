33d55d5593d349ae405351ff85ff777808a8fb70
salsaboduk <matthew.farrar@plymouth.ac.uk>
2017-06-19 12:46:24 +0100

    make logo clickable[B

--- primo-explore/custom/MATT_TEST/js/custom.js
+++ primo-explore/custom/MATT_TEST/js/custom.js
@@ -1,19 +1,27 @@
 (function () {
-    "use strict";
-    'use strict';
+  "use strict";
+  'use strict';
 
 
-    var app = angular.module('viewCustom', ['angularLoad']);
-  
+  var app = angular.module('viewCustom', ['angularLoad']);
 
-    /****************************************************************************************************/
+  app.component('prmLogoAfter',{
+  bindings: {parentCtrl: '<'},
+  controller: 'prmLogoAfterController',
+  template: `<div class="product-logo product-logo-local" layout="row" layout-align="start center" layout-fill id="banner" tabindex="0" role="banner">
+  <a href="https://www.plymouth.ac.uk">
+  <img class="logo-image" alt="{{(\'nui.header.LogoAlt\' | translate)}}" ng-src="{{$ctrl.getIconLink()}}"/>
+  </a>
+  </div>`
+});  
 
-        /*In case of CENTRAL_PACKAGE - comment out the below line to replace the other module definition*/
-
-        /*var app = angular.module('centralCustom', ['angularLoad']);*/
-
-    /****************************************************************************************************/
-
+app.controller('prmLogoAfterController', [function () {
+  var vm = this;
+  vm.getIconLink = getIconLink;
+  function getIconLink() {
+    return vm.parentCtrl.iconLink;
+  }
+}]);
 
 })();
 
@@ -23,3 +31,6 @@
   x.src = (document.location.protocol === "https:" ? "https://" : "http://") + "us.refchatter.net/js/libraryh3lp.js?310";
   var y = document.getElementsByTagName("script")[0]; y.parentNode.insertBefore(x, y);
 })();
+
+var c;
+
