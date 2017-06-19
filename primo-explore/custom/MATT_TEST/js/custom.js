f45126ea59b7068b9c9265c3404743accfe1dcbe
salsaboduk <matthew.farrar@plymouth.ac.uk>
2017-06-19 16:21:11 +0100

    end of day commit

--- primo-explore/custom/MATT_TEST/js/custom.js
+++ primo-explore/custom/MATT_TEST/js/custom.js
@@ -6,22 +6,22 @@
   var app = angular.module('viewCustom', ['angularLoad']);
 
   app.component('prmLogoAfter',{
-  bindings: {parentCtrl: '<'},
-  controller: 'prmLogoAfterController',
-  template: `<div class="product-logo product-logo-local" layout="row" layout-align="start center" layout-fill id="banner" tabindex="0" role="banner">
-  <a href="https://www.plymouth.ac.uk">
-  <img class="logo-image" alt="{{(\'nui.header.LogoAlt\' | translate)}}" ng-src="{{$ctrl.getIconLink()}}"/>
-  </a>
-  </div>`
-});  
+    bindings: {parentCtrl: '<'},
+    controller: 'prmLogoAfterController',
+    template: `<div class="product-logo product-logo-local" layout="row" layout-align="start center" layout-fill id="banner" tabindex="0" role="banner">
+    <a href="https://www.plymouth.ac.uk">
+    <img class="logo-image" alt="{{(\'nui.header.LogoAlt\' | translate)}}" ng-src="{{$ctrl.getIconLink()}}"/>
+    </a>
+    </div>`
+  });  
 
-app.controller('prmLogoAfterController', [function () {
-  var vm = this;
-  vm.getIconLink = getIconLink;
-  function getIconLink() {
-    return vm.parentCtrl.iconLink;
-  }
-}]);
+  app.controller('prmLogoAfterController', [function () {
+    var vm = this;
+    vm.getIconLink = getIconLink;
+    function getIconLink() {
+      return vm.parentCtrl.iconLink;
+    }
+  }]);
 
 })();
 
@@ -31,6 +31,4 @@ app.controller('prmLogoAfterController', [function () {
   x.src = (document.location.protocol === "https:" ? "https://" : "http://") + "us.refchatter.net/js/libraryh3lp.js?310";
   var y = document.getElementsByTagName("script")[0]; y.parentNode.insertBefore(x, y);
 })();
-
-var c;
 
