c022d4e0e9aad86722fa4ac15a2b4043228810e3
Noam Amit <noamamit92@gmail.com>
2017-09-18 07:47:28 +0300

    restore proxy default

--- gulp/config.js
+++ gulp/config.js
@@ -118,7 +118,7 @@ var SERVERS = {
 };
 
 /*Note that for SSL environments (https) define the server as: var PROXY_SERVER = https://your-server:443*/
-var PROXY_SERVER = 'http://ubz-primo.hosted.exlibrisgroup.com';
+var PROXY_SERVER = 'http://il-primo17:1703';
 
 
 let buildParams = {
