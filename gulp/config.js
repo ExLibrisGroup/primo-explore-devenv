f6af103253abc8ba099540160d899a13d8774178
Matthias Einbrodt <matthias.einbrodt@unibz.it>
2017-09-27 16:11:18 +0200

    Remove unibz PROXY_SERVER setting

--- gulp/config.js
+++ gulp/config.js
@@ -141,7 +141,7 @@ var SERVERS = {
  *   var PROXY_SERVER = 'http://abc-primo.hosted.exlibrisgroup.com'
  *   var PROXY_SERVER = 'https://abc-primo.hosted.exlibrisgroup.com:443'
  */
-var PROXY_SERVER = 'http://ubz-primo-test.hosted.exlibrisgroup.com';
+var PROXY_SERVER = 'http://your-server:your-port';
 
 
 let buildParams = {
