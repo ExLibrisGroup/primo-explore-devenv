92f9d874f319e3fa961ef36b3cfb7afbbaa6889f
Dan M <dan.moore@oregonstate.edu>
2017-10-26 12:14:11 -0700

    set up proxy

--- gulp/config.js
+++ gulp/config.js
@@ -141,8 +141,8 @@ var SERVERS = {
  *   var PROXY_SERVER = 'http://abc-primo.hosted.exlibrisgroup.com'
  *   var PROXY_SERVER = 'https://abc-primo.hosted.exlibrisgroup.com:443'
  */
-var PROXY_SERVER = 'http://your-server:your-port';
 
+var PROXY_SERVER = 'https://alliance-primo.hosted.exlibrisgroup.com:443';
 
 let buildParams = {
     customFile: customFile,
