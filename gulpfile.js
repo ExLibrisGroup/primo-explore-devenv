36004613af1153b33850ee6f0c39adde39d342ef
Noam Amit <noamamit92@gmail.com>
2017-11-02 10:28:07 +0200

    initial commit

--- gulpfile.js
+++ gulpfile.js
@@ -7,8 +7,11 @@ const config = require('./gulp/config');
 
 
 var options = minimist(process.argv.slice(2));
-config.setView(options.view);
+config.setView(options.view || 'MOCK');
 if (options.reinstallNodeModules) config.setReinstallNodeModules(options.reinstallNodeModules);
 if (options.proxy) config.setProxy(options.proxy);
 if (options.useScss) config.setUseScss(options.useScss);
 config.setBrowserify(options.browserify);
+
+
+gulp.task('web', ['serve']); //watch
