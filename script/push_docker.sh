794b0b01827109dacf6c2a6f3dcd9d7ac8d31e77
Elliott Grieco <elliott.grieco@nyu.edu>
2019-02-25 16:21:16 -0500

    Uses if statement to avoid failure

--- script/push_docker.sh
+++ script/push_docker.sh
@@ -9,7 +9,7 @@ docker push quay.io/nyulibraries/primo-explore-devenv:${CIRCLE_BRANCH//\//_}
 docker push quay.io/nyulibraries/primo-explore-devenv:${CIRCLE_BRANCH//\//_}-${CIRCLE_SHA1}
 
 # Pushes using version number for master
-[ $CIRCLE_BRANCH == master ] \
-  && echo 'hello world' \
-  && docker tag primo-explore-devenv quay.io/nyulibraries/primo-explore-devenv:$version \
-  && docker push quay.io/nyulibraries/primo-explore-devenv:$version
+if [[ $CIRCLE_BRANCH == master ]]; then
+  docker tag primo-explore-devenv quay.io/nyulibraries/primo-explore-devenv:$version
+  docker push quay.io/nyulibraries/primo-explore-devenv:$version
+fi
