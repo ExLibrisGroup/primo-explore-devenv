5acaaa0c58bf0f9e3c570bfc607fa584dd2922ea
Elliott Grieco <elliott.grieco@nyu.edu>
2019-02-25 16:17:43 -0500

    Versions image

--- script/push_docker.sh
+++ script/push_docker.sh
@@ -7,3 +7,9 @@ docker tag primo-explore-devenv quay.io/nyulibraries/primo-explore-devenv:${CIRC
 docker push quay.io/nyulibraries/primo-explore-devenv:latest
 docker push quay.io/nyulibraries/primo-explore-devenv:${CIRCLE_BRANCH//\//_}
 docker push quay.io/nyulibraries/primo-explore-devenv:${CIRCLE_BRANCH//\//_}-${CIRCLE_SHA1}
+
+# Pushes using version number for master
+[ $CIRCLE_BRANCH == master ] \
+  && echo 'hello world' \
+  && docker tag primo-explore-devenv quay.io/nyulibraries/primo-explore-devenv:$version \
+  && docker push quay.io/nyulibraries/primo-explore-devenv:$version
