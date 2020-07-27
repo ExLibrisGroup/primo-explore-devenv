3475bec7f6d75482dcc2716f6db7e6a209de2e50
Ulrich Leodolter <ulrich.leodolter@obvsg.at>
2020-07-27 15:36:22 +0200

    watch-custom-scss allowEmpty

--- gulp/tasks/03-scss.js
+++ gulp/tasks/03-scss.js
@@ -105,7 +105,7 @@ gulp.task("watch-custom-scss", gulp.series('select-view', (cb) => {
         cb();
         return;
 	}
-    gulp.watch([config.customScssDir() + "/**/*.scss"], gulp.series('custom-scss'));
+    gulp.watch([config.customScssDir() + "/**/*.scss"], {allowEmpty:true},  gulp.series('custom-scss'));
     cb();
 }));
 
