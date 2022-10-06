0ccce84f171282fd14039e385adf0d426d86512e
rdeiser <50148137+rdeiser@users.noreply.github.com>
2022-10-06 12:49:56 -0500

    Update custom.js

--- primo-explore/custom/NewUI/js/custom.js
+++ primo-explore/custom/NewUI/js/custom.js
@@ -182,9 +182,8 @@
           this.prmFacetCtrl = controller;
         },
         addExtSearch: function addExtSearch() {
-//          var xx = this;
+          var xx = this;
           var checkExist = setInterval(function () {
-            var xx = this;
   
             if (xx.prmFacetCtrl.facetService.results[0] && xx.prmFacetCtrl.facetService.results[0].name !="External Search") {
               if (xx.prmFacetCtrl.facetService.results.name !== 'External Search') {
