59e0cb972295f7c7b1da837c1e39eec544e4f807
rdeiser <50148137+rdeiser@users.noreply.github.com>
2022-10-06 11:56:28 -0500

    Update custom.js

--- primo-explore/custom/NewUI/js/custom.js
+++ primo-explore/custom/NewUI/js/custom.js
@@ -152,85 +152,55 @@
     });
   
     //Add External Search from WSU -- revised JMcWilliams 20180619
-    angular
-    .module('externalSearch', [])
-    .value('searchTargets', [])
-    .value('externalSearchText', "")
-    .component('prmFacetAfter', {
-        bindings: { parentCtrl: '<' },
-        controller: ['externalSearch', '$scope', function (externalSearch, $scope) {
-          this.$onInit = function () {
-            externalSearch.setController(this.parentCtrl)
-            externalSearch.addExtSearch();
-            $scope.$watch('$ctrl.parentCtrl.facets', function(){
-              externalSearch.addExtSearch()});
-          }
-        }]
-    })
-    .component('prmPageNavMenuAfter', {
-      controller: ['externalSearch', function (externalSearch) {
-        this.$onInit = function () {
-          if (externalSearch.getController()) externalSearch.addExtSearch()
-        }
+    angular.module('externalSearch', []).value('searchTargets', []).component('prmFacetAfterAppStoreGenerated', {
+      bindings: { parentCtrl: '<' },
+      controller: ['externalSearchService', function (externalSearchService) {
+        externalSearchService.controller = this.parentCtrl;
+        externalSearchService.addExtSearch();
       }]
-    })
-    .component('prmFacetExactAfter', {
-        bindings: { parentCtrl: '<' },
-        template: '<div ng-if="name === \'External Search\'">\
-            <div ng-hide="$ctrl.parentCtrl.facetGroup.facetGroupCollapsed">\
-                <div class="section-content animate-max-height-variable">\
-                    <div ng-if="introText" id="external-search-intro">{{introText}}</div>\
-                    <div class="md-chips md-chips-wrap">\
-                        <div ng-repeat="target in targets" aria-live="polite" class="md-chip animate-opacity-and-scale facet-element-marker-local4">\
-                            <div class="md-chip-content layout-row" role="button" tabindex="0">\
-                                <strong dir="auto" title="{{ target.name }}">\
-                                    <a ng-href="{{ target.url + target.mapping(queries, filters) }}" target="_blank">\
-                                        <img ng-src="{{ target.img }}" width="22" height="22" alt="{{ target.alt }}" style="vertical-align:middle;"> {{ target.name }}\
-                                    </a>\
-                                </strong>\
-                            </div>\
-                        </div>\
-                    </div>\
-                </div>\
-            </div>\
-        </div>',
-        controller: ['$scope', '$location', 'searchTargets', 'externalSearchText', function ($scope, $location, searchTargets, externalSearchText) {
-          this.$onInit = function () {
-            $scope.name = this.parentCtrl.facetGroup.name
-            $scope.targets = searchTargets
-            $scope.introText = externalSearchText
-            var query = $location.search().query
-            var filter = $location.search().pfilter
-            $scope.queries = Object.prototype.toString.call(query) === '[object Array]' ? query : query ? [query] : false
-            $scope.filters = Object.prototype.toString.call(filter) === '[object Array]' ? filter : filter ? [filter] : false
-          }
-        }]
-    })
-    .factory('externalSearch', function () {
+    }).component('prmPageNavMenuAfterAppStoreGenerated', {
+      controller: ['externalSearchService', function (externalSearchService) {
+        if (externalSearchService.controller) externalSearchService.addExtSearch();
+      }]
+    }).component('prmFacetExactAfterAppStoreGenerated', {
+      bindings: { parentCtrl: '<' },
+      template: '\n      <div ng-if="name === \'External Search\'">\n          <div ng-hide="$ctrl.parentCtrl.facetGroup.facetGroupCollapsed">\n              <div class="section-content animate-max-height-variable">\n                  <div class="md-chips md-chips-wrap">\n                      <div ng-repeat="target in targets" aria-live="polite" class="md-chip animate-opacity-and-scale facet-element-marker-local4">\n                          <div class="md-chip-content layout-row" role="button" tabindex="0">\n                              <strong dir="auto" title="{{ target.name }}">\n                                  <a ng-href="{{ target.url + target.mapping(queries, filters) }}" target="_blank">\n                                      <img ng-src="{{ target.img }}" width="22" height="22" alt="{{ target.alt }}" style="vertical-align:middle;"> {{ target.name }}\n                                  </a>\n                              </strong>\n                          </div>\n                      </div>\n                  </div>\n              </div>\n          </div>\n      </div>',
+      controller: ['$scope', '$location', 'searchTargets', function ($scope, $location, searchTargets) {
+        $scope.name = this.parentCtrl.facetGroup.name;
+        $scope.targets = searchTargets;
+        var query = $location.search().query;
+        var filter = $location.search().pfilter;
+        $scope.queries = Array.isArray(query) ? query : query ? [query] : false;
+        $scope.filters = Array.isArray(filter) ? filter : filter ? [filter] : false;
+      }]
+    }).factory('externalSearchService', function () {
       return {
-        getController: function () {
-          return this.prmFacetCtrl || false
+        get controller() {
+          return this.prmFacetCtrl || false;
         },
-        setController: function (controller) {
-          this.prmFacetCtrl = controller
+        set controller(controller) {
+          this.prmFacetCtrl = controller;
         },
-        addExtSearch: function addExtSearch(ctrl) {
-          if ( !ctrl ) ctrl = this.prmFacetCtrl;
-          if ( 
-            ctrl.facets.length < 1 ||
-            ctrl.facets[0].name !== 'External Search' 
-            ) {
-            ctrl.facets.unshift({
-              name: 'External Search',
-              displayedType: 'exact',
-              limitCount: 0,
-              facetGroupCollapsed: false,
-              values: []
-            });
-          }
+        addExtSearch: function addExtSearch() {
+          var xx = this;
+          var checkExist = setInterval(function () {
+  
+            if (xx.prmFacetCtrl.facetService.results[0] && xx.prmFacetCtrl.facetService.results[0].name !="External Search") {
+              if (xx.prmFacetCtrl.facetService.results.name !== 'External Search') {
+                xx.prmFacetCtrl.facetService.results.unshift({
+                  name: 'External Search',
+                  displayedType: 'exact',
+                  limitCount: 0,
+                  facetGroupCollapsed: false,
+                  values: undefined
+                });
+              }
+              clearInterval(checkExist);
+            }
+          }, 100);
         }
-      }
-    })
+      };
+    });
     app.value('searchTargets', [{
       "name": "Worldcat",
       "url": "https://kansasstateuniversity.on.worldcat.org/search?",
